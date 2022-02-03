1ff594612bb238910b45cb3e8fee982f2c0d814a
Arish Ahmad <67474371+arish-ahmad@users.noreply.github.com>
2022-02-03 19:39:08 +0530

    Update Assignment3.java

--- Java Fundamentals/Language Basics/Assignment3.java
+++ Java Fundamentals/Language Basics/Assignment3.java
@@ -1,3 +1,8 @@
+/*Write a Program to accept two integers as command line arguments and print the sum of the two numbers
+Example1)
+C:\>java Sample 10 20
+O/P Expected : The sum of 10 and 20 is 30
+*/
 public class Assignment3 {
 
 	public static void main(String[] args) {
